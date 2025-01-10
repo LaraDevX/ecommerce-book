9f428b47c062fdb8223f7223c88f261ddde110ba
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-10 21:44:08 +0500

    Created interfaces for user and book

---
+++ app/Interfaces/Services/BookServiceInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Interfaces\Services;
+
+class BookServiceInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ app/Interfaces/Services/UserServiceInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Interfaces\Services;
+
+class UserServiceInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
