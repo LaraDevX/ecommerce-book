22a860cd50142b283d008be937cafa10839bb276
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-11 21:57:47 +0500

    Binding interfaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
+use App\Interfaces\Services\UserServiceInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(UserServiceInterface::class, UserService::class);
+        // $this->app->bind(UserServiceInterface::class, UserService::class);
     }
 
     /**
