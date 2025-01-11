e1e953a513014252b8857c04cc8079c944a23a47
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-11 21:14:45 +0500

    Added full code to AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Traits\ResponseTrait;
+use App\Interfaces\Services\UserServiceInterface;
 
 class AuthController extends Controller
 {
-    //
+    use ResponseTrait;
+    public function __construct(protected UserServiceInterface $userService){
+
+    }
+    public function register(RegisterRequest $request){
+
+        $user = $this->userService->registerUser($request->all());
+        SendEmailJob::dispatch($user);
+        return $this->success(new UserResource($user), __('successes.user.created'), 201);
+    }
+    public function login(LoginRequest $request){
+        $token = $this->userService->loginUser($request->all());
+        return $this->success($token, __('successes.user.logged'));
+    }
+    public function findUser(Request $request){
+        return $this->success(new UserResource($request->user()));
+    }
+    public function verifyEmail(Request $request){
+        $message = $this->userService->verifyEmail($request->token);
+        return $this->success([], $message);
+    }
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+        return $this->success([], __('successes.user.logged_out'), 204);
+    }
 }
