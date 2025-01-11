3cf95442a643a0e88d026253516fb0d3e5d4b1b5
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-11 22:03:48 +0500

    Changed

--- database/migrations/2025_01_10_171019_add_role_id_to_users.php
+++ database/migrations/2025_01_10_171019_add_role_id_to_users.php
@@ -12,7 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('role_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('role_id')
+            ->nullable()
+            ->default(2)
+            ->constrained()
+            ->cascadeOnDelete();
         });
     }
 
