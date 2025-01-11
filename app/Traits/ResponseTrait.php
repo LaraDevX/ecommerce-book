753c12fa6ba294a70df8f0a4652e3373f778aa0d
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-11 21:15:30 +0500

    Added full code to ResponseTrait

--- app/Traits/ResponseTrait.php
+++ app/Traits/ResponseTrait.php
@@ -2,13 +2,53 @@
 
 namespace App\Traits;
 
-class ResponseTrait
+use Illuminate\Pagination\LengthAwarePaginator;
+
+trait ResponseTrait
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    protected function success($data = [], string $message = null, int $status = 200)
     {
-        //
+        $message = $message ?: __('successes.operation');
+        return response()->json([
+            'status' => __('successes.status'),
+            'message' => $message,
+            'data' => $data,
+        ], $status);
     }
+    protected function responsePagination($paginator, $data = [], string $message = null, int $status = 200)
+    {
+        if ($paginator instanceof LengthAwarePaginator) {
+            $pagination = [
+                'current_page' => $paginator->currentPage(),
+                'total_pages' => $paginator->lastPage(),
+                'total' => $paginator->total(),
+                'per_page' => $paginator->perPage(),
+                'links' => [
+                    'first' => $paginator->url(1),
+                    'last' => $paginator->url($paginator->lastPage()),
+                    'prev' => $paginator->previousPageUrl(),
+                    'next' => $paginator->nextPageUrl(),
+                ],
+            ];
+        } else {
+            $pagination = null;
+        }
+
+        $message = $message ?: __('successes.operation');
+        return response()->json([
+            'status' => __('successes.status'),
+            'message' => $message,
+            'data' => $data,
+            'pagination' => $pagination,
+        ], $status);
+    }
+    protected function error(string $message = null, int $status = 400)
+    {
+        $message = $message ?: __('errors.operation');
+        return response()->json([
+            'status' => __('errors.status'),
+            'message' => $message,
+        ], $status);
+    }
+
 }
