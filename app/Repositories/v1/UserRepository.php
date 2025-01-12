38389be89463984f3ca1a8fef94810533007a35a
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-12 19:03:34 +0500

    Created repositories in the folder v1

---
+++ app/Repositories/v1/UserRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Jobs\SendEmailJob;
+use App\Models\User;
+use Illuminate\Support\Str;
+use App\Interfaces\Repositories\UserRepositoryInterface;
+
+class UserRepository implements UserRepositoryInterface
+{
+    public function createUser($data){
+        $user = new User();
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->password = $data['password'];
+        $user->verification_token = $data['verification_token'];
+        $user->save();
+        SendEmailJob::dispatch($user);
+        return $user->load('role');
+    }
+    public function getUserByEmail($email){
+        return User::where('email', $email)->firstOrFail();
+    }
+    public function findUserByToken($token){
+        $user = User::where('verification_token', $token)->firstOrFail();
+        $user->email_verified_at = now();
+        $user->save();
+        return $user->with('role');
+    }
+}
