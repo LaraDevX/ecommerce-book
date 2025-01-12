b04eab99a159a99563317bfe3310b5f40bfb3525
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-12 18:58:32 +0500

    Created controllers auth & book in the folder v1

---
+++ app/Http/Controllers/v1/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\SendEmailJob;
+use Illuminate\Http\Request;
+use App\Traits\ResponseTrait;
+use App\Http\Resources\UserResource;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Interfaces\Services\UserServiceInterface;
+
+class AuthController extends Controller
+{
+    use ResponseTrait;
+    public function __construct(protected UserServiceInterface $userService){
+
+    }
+    public function register(RegisterRequest $request){
+
+        $user = $this->userService->registerUser($request->all());
+        return $this->success(new UserResource($user), __('successes.user.created'), 201);
+    }
+    public function login(LoginRequest $request){
+        $token = $this->userService->loginUser($request->all());
+        return $this->success($token, __('successes.user.logged'));
+    }
+    public function findUser(Request $request){
+        return $this->success(new UserResource($request->user()));
+    }
+    public function verifyEmail(Request $request){
+        $message = $this->userService->verifyEmail($request->token);
+        return $this->success([], $message);
+    }
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+        return $this->success([], __('successes.user.logged_out'), 204);
+    }
+}
