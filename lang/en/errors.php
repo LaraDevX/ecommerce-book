0d1dd942a21710fe11f3dc46ab2e08d4fbead684
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-16 16:18:59 +0500

    Added translations for errors, successes & validation in the language english

---
+++ lang/en/errors.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'no' => 'Not found',
+    'status' => 'Error',
+    'operation' => 'An error occured',
+    'book' => [
+        'forbidden' => 'This book is not yours',
+        'not_found' => 'Book not found',
+    ],
+    '404' => 'Page not found',
+    'model' => [
+        'not_found' => 'not found',
+    ],
+    'email' => [
+        'not_verified' => 'Email not verified.',
+    ],
+    'user' => [
+        'not_found' => 'User not found or password is incorrect.'
+    ]
+];
---
+++ lang/en/successes.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'status' => 'Success!',
+    'operation' => 'Operation successfully!',
+    'user' => [
+        'created' => 'User created successfully!',
+        'logged_out' => 'User logged out successfully!',
+        'logged' => 'User logged successfully!',
+    ],
+    'book' => [
+        'created' => 'Book created successfully!',
+        'updated' => 'Book updated successfully!',
+        'deleted' => 'Book deleted successfully!',
+    ],
+    'email' => [
+        'verified' => 'Email verified successfully!',
+    ],
+];
