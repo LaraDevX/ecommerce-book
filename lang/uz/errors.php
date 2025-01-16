2935e7c7e2515759a7faa0be675143a41e2954e0
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-16 16:19:24 +0500

    Added translations for errors, successes & validation in the language uzbek

---
+++ lang/uz/errors.php
@@ -0,0 +1,18 @@
+<?php
+
+    return [
+        'no' => "Ma'lumot topilmadi",
+        'status' => 'Muvaffaqiyatsiz',
+        'operation' => 'Xatolik yuz berdi',
+        'book' => [
+            'forbidden' => 'Kitob sizniki emas',
+            'not_found' => 'Kitob topilmadi'
+        ],
+        '404' => 'Sahifa topilmadi',
+        'model' => [
+            'not_found' => 'topilmadi'
+        ],
+        'email' => [
+            'not_verified' => 'Email tasdiqlanmagan.'
+        ]
+    ];
---
+++ lang/uz/successes.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'status' => 'Muvaffaqiyatli',
+    'operation' => 'Operatsiya muvaffaqiyatli bajarildi!',
+    'user' => [
+        'created' => 'Foydalanuvchi muvaffaqiyatli yaratildi!',
+        'logged' => 'Foydalanuvchi tizimga muvaffaqiyatli kirdi!',
+        'logged_out' => 'Foydalanuvchi tizimdan muvaffaqiyatli chiqdi!',
+    ],
+    'book' => [
+        'created' => 'Kitob muvaffaqiyatli yaratildi!',
+        'updated' => 'Kitob muvaffaqiyatli yangilandi!',
+        'deleted' => "Kitob muvaffaqiyatli o'chirildi",
+    ],
+    'email' => [
+        'verified' => 'Email muvaffaqiyatli tasdiqlandi!',
+    ],
+];
