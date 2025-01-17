de6f90091618977ae2a2585ad72e02934fbb01bc
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-17 23:42:45 +0500

    Removed method nullable from migration role_id

--- database/migrations/2025_01_10_171019_add_role_id_to_users.php
+++ database/migrations/2025_01_10_171019_add_role_id_to_users.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->foreignId('role_id')
-            ->nullable()
             ->default(2)
             ->constrained()
             ->cascadeOnDelete();
