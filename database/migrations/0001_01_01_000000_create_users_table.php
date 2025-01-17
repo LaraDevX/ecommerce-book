c29a5e26a2666298566ffba7d69f310fa73fbf4f
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-17 23:47:26 +0500

    Updated

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->foreignId('role_id')
-                ->default(2)
-                ->constrained()
-                ->cascadeOnDelete();
+            ->constrained()
+            ->cascadeOnDelete()
+            ->default(2);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
