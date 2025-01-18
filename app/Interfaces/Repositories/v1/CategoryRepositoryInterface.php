5e553807f1dcc04e45f267d06af8b51d2e07cd2c
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-18 15:36:39 +0500

    Created interface for category

---
+++ app/Interfaces/Repositories/v1/CategoryRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Interfaces\Repositories\v1;
+
+class CategoryRepositoryInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
