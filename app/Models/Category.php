57a0a5eeb2ac472be2e03f6744d32f2581e44805
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-18 14:54:12 +0500

    Added all relation method fillables

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Category extends Model implements TranslatableContract
 {
-    //
+    use Translatable;
+    public $translatedAttributes = ['name'];
+    protected $fillable = ['slug', 'parent_id'];
+    public function parent(){
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+    public function children(){
+        return $this->hasMany(Category::class, 'parent_id');
+    }
 }
