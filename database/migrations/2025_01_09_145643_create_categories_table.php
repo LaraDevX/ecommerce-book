5a023fb15bbd8a0cdd7c5cecc2528d1501057a8a
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-18 14:27:36 +0500

    Added columns to categories

--- database/migrations/2025_01_09_145643_create_categories_table.php
+++ database/migrations/2025_01_09_145643_create_categories_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('parent_id')->constrained('categories');
+            $table->string('slug');
             $table->timestamps();
         });
     }
