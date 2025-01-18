4585fd4f64a9c7c794f3743ff057f8a960cdb68f
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-18 15:36:17 +0500

    Created interfaces for category

---
+++ app/Interfaces/Services/v1/CategoryServiceInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Interfaces\Services\v1;
+
+class CategoryServiceInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
