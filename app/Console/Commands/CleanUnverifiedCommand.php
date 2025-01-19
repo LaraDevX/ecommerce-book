fb552e8b0c6446ef2c8ad1d2f6bd4b7a04c3415a
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-19 23:27:38 +0500

    Created command for clean unverified users

---
+++ app/Console/Commands/CleanUnverifiedCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CleanUnverifiedCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:clean-unverified-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clean unverified users';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        
+    }
+}
