4f3f39bd618b11fda79f75fdc5f956e426484c11
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-23 21:24:53 +0500

    Added full code

--- app/Http/Resources/v1/CategoryResource.php
+++ app/Http/Resources/v1/CategoryResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\v1;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\v1\BookResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CategoryResource extends JsonResource
@@ -14,6 +15,11 @@ class CategoryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'children' => $this->whenLoaded('children'),
+            'books' => BookResource::collection($this->whenLoaded('books'))
+        ];
     }
 }
