d78aae47a9cf0ccfc6d2010935e68e3e2da0284a
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-23 21:19:43 +0500

    Updated

--- app/Http/Controllers/v1/CategoryController.php
+++ app/Http/Controllers/v1/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\v1;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\v1\CategoryResource;
 use App\Http\Requests\StoreCategoryRequest;
 
 class CategoryController extends Controller
