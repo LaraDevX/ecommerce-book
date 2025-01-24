cefd26519360d4e69503e0044cad053d55770819
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-24 17:00:27 +0500

    Added fillable and relation methods to model Attachment

--- database/migrations/2025_01_10_164035_create_attachments_table.php
+++ database/migrations/2025_01_10_164035_create_attachments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('attachments', function (Blueprint $table) {
             $table->id();
+            $table->string('extra_identifier')->nullable();
+            $table->string('title')->nullable();
+            $table->string('path');
+            $table->string('size');
+            $table->string('type')->nullable();
+            $table->nullableMorphs('attachment');
             $table->timestamps();
         });
     }
