275d528b1b363b96c9d2dbb93833b5a1215067d1
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-26 19:58:15 +0500

    Added full code to service

--- app/Services/v1/CategoryService.php
+++ app/Services/v1/CategoryService.php
@@ -2,22 +2,34 @@
 
 namespace App\Services\v1;
 
+use App\Interfaces\Repositories\v1\CategoryRepositoryInterface;
+
 class CategoryService
 {
-    public function allCategories($perPage){
+    public function __construct(protected CategoryRepositoryInterface $categoryRepository){
 
     }
+    public function allCategories($perPage){
+        $categories = $this->categoryRepository->allCategories();
+        return $categories;
+    }
     public function createCategory($data){
-
+        $category = $this->categoryRepository->createCategory($data);
+        return $category;
     }
 
     public function getCategory($id){
-
+        $category = $this->categoryRepository->getCategory($id);
+        return $category;
     }
     public function updateCategory($data, $id){
-
+        $category = $this->categoryRepository->getCategory($id);
+        $updatedCategory = $this->categoryRepository->updateCategory($category);
+        return $updatedCategory;
     }
     public function deleteCategory($id){
-        
+        $category = $this->categoryRepository->getCategory($id);
+        $this->categoryRepository->deleteCategory($category);
+        return;
     }
 }
