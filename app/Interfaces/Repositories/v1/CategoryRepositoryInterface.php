af544e573e8838b6b4546b3f651b6620c647e963
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-26 19:58:01 +0500

    Added all unbodied methods to interface

--- app/Interfaces/Repositories/v1/CategoryRepositoryInterface.php
+++ app/Interfaces/Repositories/v1/CategoryRepositoryInterface.php
@@ -4,5 +4,9 @@ namespace App\Interfaces\Repositories\v1;
 
 interface CategoryRepositoryInterface
 {
-    
+    public function allCategories();
+    public function getCategory($id);
+    public function updateCategory($data);
+    public function createCategory($data);
+    public function deleteCategory($category);
 }
