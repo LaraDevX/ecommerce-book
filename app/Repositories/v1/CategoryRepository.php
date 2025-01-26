d8814d9267578daecd82d32788d61a14ddb4e648
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-26 20:03:54 +0500

    Added full code to repository

--- app/Repositories/v1/CategoryRepository.php
+++ app/Repositories/v1/CategoryRepository.php
@@ -1,14 +1,37 @@
 <?php
-
 namespace App\Repositories\v1;
+
+use App\Models\Category;
 
 class CategoryRepository
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public function allCategories()
     {
-        //
+        $categories = Category::with('books')->paginate(20);
+        return $categories;
+    }
+    public function createCategory($data)
+    {
+        $category       = new Category();
+        $category->name = $data['name'];
+        $category->save();
+        return $category;
+    }
+    public function getCategory($id)
+    {
+        $category = Category::with('books')->findOrFail($id);
+        return $category;
+    }
+    public function updateCategory($data)
+    {
+        $category = new Category();
+        $category->name = $data['name'];
+        $category->save();
+        return $category;
+    }
+    public function deleteCategory($category)
+    {
+        $category->delete();
+        return;
     }
 }
