e745ed7c56920d91f4f80f8afface26a563ef975
Dilshoda <qambarovadilshoda867@gmail.com>
2025-01-27 14:53:03 +0500

    Created request classes for auth & category

---
+++ app/Http/Requests/v1/Category/UpdateCategoryRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\v1\Category;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            [
+                'translations.en.name' => 'sometimes|string',
+                'translations.ru.name' => 'sometimes|string',
+                'translations.uz.name' => 'sometimes|string',
+
+                'parent_id' => 'nullable|exists:categories,id'
+            ]
+        ];
+    }
+}
